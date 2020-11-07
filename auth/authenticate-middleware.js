e3bf5412cf28a30a86b168b8660bd2e14ae5e0a3
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-11-07 12:01:57 -0500

    auth middleware complete

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,43 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const token = require("jsonwebtoken")
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+function restrict() {
+  return async (req, res, next) =>{
+    try{
+
+      const token = req.cookies.token
+
+      if(!token) {
+        return res.status(401).json({
+          message: "Invalid credentials"
+        })
+      }
+
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).json({
+            message: "Invalid credentials"
+          })
+        }
+
+        req.token = decoded
+
+        next()
+      })
+    }
+    catch(err){
+
+    }
+  }
+}
+
+
+// module.exports = (req, res, next) => {
+//   res.status(401).json({ you: 'shall not pass!' });
+// };
+
+module.exports = {
+  restrict()
+}
